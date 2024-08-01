b5daef4ed08ded966d8e6d2cda73af64b88d7519
황호윤 <hoyoon9227@gmail.com>
2024-08-01 23:12:48 +0900

    logback loki 옵션추가

---
+++ src/main/java/com/skybridge/common/filter/LoggingFilter.java
@@ -0,0 +1,30 @@
+package com.skybridge.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class LoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final UUID uuid = UUID.randomUUID();
+        String requestId = ((HttpServletRequest)servletRequest).getHeader("X-RequestID");
+        MDC.put("compose_service", "app-skybridge-service");
+        MDC.put("trace_id", StringUtils.defaultIfEmpty(requestId, UUID.randomUUID().toString().replace("-", "")));
+        MDC.put("span_id", uuid.toString());
+        MDC.put("trace_flags", "01");
+        MDC.put("type", "request");
+        filterChain.doFilter(servletRequest, servletResponse);
+        MDC.clear();
+    }
+}
