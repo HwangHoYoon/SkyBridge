88d1e8357855f39d9edd76c28c81388354f8a77e
황호윤 <hoyoon9227@gmail.com>
2024-08-13 16:13:01 +0900

    배치 시간 변경

--- src/main/java/com/skybridge/news/controller/NewsController.java
+++ src/main/java/com/skybridge/news/controller/NewsController.java
@@ -36,7 +36,7 @@ public class NewsController {
         return newsService.loadImage(id);
     }
 
-    @Scheduled(cron = "0 38 15 * * ?")
+    @Scheduled(cron = "0 20 16 * * ?")
     public void runTask() throws Exception {
         log.info("NewsService Scheduled start");
         newsService.newsScheduled();
